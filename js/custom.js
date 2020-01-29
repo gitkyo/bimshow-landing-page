717003780ddc57cd60116bdce0964047abc12e03
gitkyo <p.delaunay1@gmail.com>
2020-01-29 10:08:20 +0400

    init all files

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+(function($) {
+	'use strict';
+	/*
+	tooltip
+	=========================== */
+	$('.tooltips').tooltip({
+		selector: "a[data-toggle^=tooltip]"
+	})	
+
+	/* Client logo hover
+	=========================== */	
+	$(".logo-hover").css({'opacity':'0','filter':'alpha(opacity=0)'});	
+	$('.client-link').hover(function(){
+				$(this).find('.logo-hover').stop().fadeTo(900, 1);
+				$(this).find('.client-logo').stop().fadeTo(900, 0);
+	}, function() {
+				$(this).find('.logo-hover').stop().fadeTo(900, 0);
+				$(this).find('.client-logo').stop().fadeTo(900, 1);
+	});	
+
+	
+	
+	
+	baguetteBox.run('.gallery');
+
+
+
+	/* gellerie lightbox effect
+	=========================== */	
+	// View an image
+	//const viewer = new Viewer(document.getElementById('image'));
+
+	// View a list of images
+	//const gallery = new Viewer(document.getElementById('images'));
+
+
+
+
+})(jQuery);
---
+++ js/forme.js
@@ -0,0 +1,16 @@
+class forme {
+  constructor(x, y, val) {
+    this.x = x;
+    this.y = y;
+   
+  }
+  show() {
+      
+    rect(this.x + this.tail, this.y+ this.tail, this.x, this.y );
+    //this.x+=this.val;
+
+  }
+  move() {
+   
+  }
+}
